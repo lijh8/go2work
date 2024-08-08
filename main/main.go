72a5b841ac0bd5597ae49c91bd97cbb6523328e0
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-08-08 16:57:24 +0800

    Add files via upload

--- main/main.go
+++ main/main.go
@@ -1,10 +1,18 @@
 package main
 
-import "tuple2"
+import (
+	"log2"
+	"tuple2"
+)
 
 func main() {
+	filename := "logfile.log"
+	logfd := log2.InitLog(filename)
+	defer logfd.Close()
+
 	s1 := []any{1, "hello", 3.14, true}
 	s2 := []any{1, "hello", 3.14, true}
 	b, err := tuple2.Cmp(s1, s2)
-	println(b, err)
+	println(b, err == nil)
+
 }
