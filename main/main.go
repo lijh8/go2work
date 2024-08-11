65a18c6e1e8ce20dbcae4e575269826b9da530ae
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-08-11 10:49:03 +0800

    Add files via upload

--- main/main.go
+++ main/main.go
@@ -2,22 +2,12 @@ package main
 
 import (
 	"fmt"
-	"reflect"
+	"tuple2"
 )
 
 func main() {
-	{
-
-		// a := []any{"abc", 123, 3.14, []any{"abc", 1234, "aaa"}}
-		// b := []any{"abc", 123, 3.14, []any{"abc", 123, 3.14}}
-		// c, ok := tuple2.Cmp(a, b)
-		// fmt.Println(c, ok)
-	}
-
-	{
-		a := [...]any{"Sat", "Sun"}
-		b := []any{"Sat", "Sun"}
-		fmt.Println(reflect.TypeOf(a))
-		fmt.Println(reflect.TypeOf(b))
-	}
+	a := []any{"abc", 123, 3.14}
+	b := []any{"abc", 123, 3.14}
+	c, ok := tuple2.Cmp(a, b)
+	fmt.Println(c, ok)
 }
