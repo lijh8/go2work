dfaa58aedb0e53cc6e37942509042560c2cadd95
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-08-13 06:49:10 +0800

    Add files via upload

--- main/main.go
+++ main/main.go
@@ -1,17 +1,40 @@
 package main
 
-import (
-	"log"
-	"log2"
-)
+import "fmt"
 
 func main() {
-	logfile := "logfile.log"
-	logfd, err := log2.InitLog(logfile)
-	if err == nil {
-		defer logfd.Close()
+	ch := make(chan int, 3)
+	done := make(chan struct{})
+
+	//sending
+	var sending = func() {
+		for i := 0; i <= 5; i++ {
+			select {
+			case ch <- i:
+				fmt.Printf("sending:%d, ", i)
+			default:
+				// block on channel is full, unless there is a receiver
+				fmt.Printf("blocking, len:%d, cap:%d, ", len(ch), cap(ch))
+			}
+		}
+
+		close(ch)
+		done <- struct{}{}
 	}
 
-	log.Println("aaa")
-	log.Println("bbb")
+	//receiving
+	var receiving = func() {
+		// turn of this range loop to see the block when channel is full
+		for i := range ch {
+			fmt.Printf("receiving, ch:%d, len:%d, cap:%d, ", i, len(ch), cap(ch))
+		}
+
+		done <- struct{}{}
+	}
+
+	go sending()
+	go receiving()
+
+	<-done // one for sending
+	<-done // one for receiving
 }
