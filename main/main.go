28bbc4a55189afb4b47196a0b3bb169835907eac
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-08-13 00:23:32 +0800

    Add files via upload

--- main/main.go
+++ main/main.go
@@ -1,26 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"log2"
 )
 
 func main() {
-	// logfile := "logfile.log"
-	// logfd, err := log2.InitLog(logfile)
-	// if err == nil {
-	// 	defer logfd.Close()
-	// }
+	logfile := "logfile.log"
+	logfd, err := log2.InitLog(logfile)
+	if err == nil {
+		defer logfd.Close()
+	}
 
-	// log.Println("aaa")
-	// log.Println("bbb")
-
-	cnt := 0
-	go func() {
-		for {
-			cnt++
-			fmt.Print(cnt, ",")
-		}
-	}()
-	select {} // Block forever
-
+	log.Println("aaa")
+	log.Println("bbb")
 }
