6c94b27b14463984304fcc51906a767a77d5626e
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-08-15 06:04:49 +0800

    Add files via upload

--- main/main.go
+++ main/main.go
@@ -1,24 +1,26 @@
 package main
 
-var c = make(chan int, 3)
+// var c = make(chan int)     // ok
+var c = make(chan int, 10) // no, buffered
 var a string
-var done = make(chan any)
+var done chan any = make(chan any)
 
 func f() {
 	a = "hello, world"
-	println("aaa", len(c), cap(c))
-	v := <-c
 
-	println("bbb", len(c), cap(c), v)
+	println("len:", len(c), ",", "cap:", cap(c)) // values in c is sent in main()
+	<-c
 
 	done <- nil
 }
 
 func main() {
 	go f()
-	c <- 10
-	c <- 20
+	c <- 0
+	c <- 0 // try to send one more, and see len in f()
 
-	println("ccc", a)
 	<-done
+
+	print(a)
+
 }
