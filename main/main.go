1d32a4e11e423805703800caf70dec9418402a61
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-08-15 01:36:40 +0800

    Add files via upload

--- main/main.go
+++ main/main.go
@@ -1,65 +1,24 @@
 package main
 
-import "fmt"
+var c = make(chan int, 3)
+var a string
+var done = make(chan any)
 
-/*
-func main() {
-	// synchronize two goroutines with two channels
-	// let the code in the first goroutine execute first
-	const MAX = 6
-	cA := make(chan any)
-	cB := make(chan any)
-	done := make(chan any)
+func f() {
+	a = "hello, world"
+	println("aaa", len(c), cap(c))
+	v := <-c
 
-	go func() {
-		for i := 0; i < MAX; i++ {
-			fmt.Println("a", i)
-			cA <- nil
-			<-cB
-		}
-		done <- nil
-	}()
+	println("bbb", len(c), cap(c), v)
 
-	go func() {
-		for i := 0; i < MAX; i++ {
-			<-cA
-			fmt.Println("b", i)
-			cB <- nil
-		}
-		done <- nil
-	}()
-
-	<-done
-	<-done
+	done <- nil
 }
-*/
 
 func main() {
-	// synchronize two goroutines with two channels
-	// let the code in the second goroutine execute first
-	const MAX = 6
-	cA := make(chan any)
-	cB := make(chan any)
-	done := make(chan any)
+	go f()
+	c <- 10
+	c <- 20
 
-	go func() {
-		for i := 0; i < MAX; i++ {
-			<-cA
-			fmt.Println("a", i)
-			cB <- nil
-		}
-		done <- nil
-	}()
-
-	go func() {
-		for i := 0; i < MAX; i++ {
-			fmt.Println("b", i)
-			cA <- nil
-			<-cB
-		}
-		done <- nil
-	}()
-
-	<-done
+	println("ccc", a)
 	<-done
 }
