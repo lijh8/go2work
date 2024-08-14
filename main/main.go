ae5d4d7a7800719cf561bf7594ee2058445e8552
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-08-14 09:05:31 +0800

    Add files via upload

--- main/main.go
+++ main/main.go
@@ -1,17 +1,68 @@
 package main
 
-import (
-	"log"
-	"log2"
-)
+import "fmt"
 
 func main() {
-	logfile := "logfile.log"
-	logfd, err := log2.InitLog(logfile)
-	if err == nil {
-		defer logfd.Close()
-	}
+	ab()
+	ba()
+}
 
-	log.Println("aaa")
-	log.Println("bbb")
+// show "a","b" alternatively with two goroutines
+func ab() {
+	const MAX = 6
+	cA := make(chan any)
+	cB := make(chan any)
+	done := make(chan any)
+
+	go func() {
+		for i := 0; i < MAX; i++ {
+			fmt.Println("a", i)
+			cA <- nil
+			<-cB
+		}
+		done <- nil
+	}()
+
+	go func() {
+		for i := 0; i < MAX; i++ {
+			<-cA
+			fmt.Println("b", i)
+			cB <- nil
+		}
+		done <- nil
+	}()
+
+	<-done
+	<-done
+	fmt.Println("---")
+}
+
+// show "b","a" alternatively with two goroutines
+func ba() {
+	const MAX = 6
+	cA := make(chan any)
+	cB := make(chan any)
+	done := make(chan any)
+
+	go func() {
+		for i := 0; i < MAX; i++ {
+			fmt.Println("b", i)
+			cA <- nil
+			<-cB
+		}
+		done <- nil
+	}()
+
+	go func() {
+		for i := 0; i < MAX; i++ {
+			<-cA
+			fmt.Println("a", i)
+			cB <- nil
+		}
+		done <- nil
+	}()
+
+	<-done
+	<-done
+	fmt.Println("---")
 }
