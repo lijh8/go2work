cebbf1d74258f841d8359d8424eeeef18b762f3e
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-08-17 22:17:39 +0800

    Add files via upload

--- main/main.go
+++ main/main.go
@@ -9,16 +9,17 @@ func main() {
 		a := [3]int{1, 2, 3}
 		b := a[:] // ok, slice
 		b[0] = 100
-		fmt.Println(a, b, a[:])
+		fmt.Println(a, b)
 	}
 
 	{
-		// string is immutable,
-		// needs a type conversion explicitly
-		// converting string to slice makes a copy
+		// string is immutable like the string literal in c language.
+		// it needs a type conversion to slice explicitly.
+		// char []a = "hello";
 		a := "abc"
 		b := []rune(a) // ok, slice,
 		b[0] = 'A'
-		fmt.Println(a, string(b), string(a[:]))
+		fmt.Println(a, string(b))
+
 	}
 }
