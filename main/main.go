e2413ab7b540758d981d8991d2150bc71696c11d
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-08-18 06:36:19 +0800

    Add files via upload

--- main/main.go
+++ main/main.go
@@ -1,38 +1,22 @@
 package main
 
+import (
+	"log"
+)
+
+func f(s []int) {
+	s = append(s, 10, 20, 30)
+	log.Println(s)
+}
+
 func main() {
-	{
-		a := make([]int, 3)
-		a = append(a, 1, 2, 3)
-		println(a)
-	}
-	{
-		a := []int{1, 2, 3}
-		println(a)
-	}
-	{
-		a := make(map[string]int)
-		a["aaa"] = 100
-	}
-	{
-		a := map[string]int{"aaa": 100}
-		println(a)
-	}
+	// logfile := "logfile.log"
+	// logfd, err := log2.InitLog(logfile)
+	// if err == nil {
+	// 	defer logfd.Close()
+	// }
 
-	type T struct {
-		name string
-		num  int
-	}
-
-	{
-		a := new(T)
-		a.name = "aaa"
-		a.num = 100
-		println(a)
-	}
-	{
-		a := &T{name: "aaa", num: 100}
-		a.name = "bbb"
-		println(a)
-	}
+	s := []int{}
+	f(s)
+	log.Println(s)
 }
