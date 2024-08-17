746a1d7a3b6306ef2e8b089eeb6e3de0c108bc87
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-08-18 03:27:38 +0800

    Add files via upload

--- main/main.go
+++ main/main.go
@@ -1,5 +1,38 @@
 package main
 
 func main() {
-	println("main")
+	{
+		a := make([]int, 3)
+		a = append(a, 1, 2, 3)
+		println(a)
+	}
+	{
+		a := []int{1, 2, 3}
+		println(a)
+	}
+	{
+		a := make(map[string]int)
+		a["aaa"] = 100
+	}
+	{
+		a := map[string]int{"aaa": 100}
+		println(a)
+	}
+
+	type T struct {
+		name string
+		num  int
+	}
+
+	{
+		a := new(T)
+		a.name = "aaa"
+		a.num = 100
+		println(a)
+	}
+	{
+		a := &T{name: "aaa", num: 100}
+		a.name = "bbb"
+		println(a)
+	}
 }
