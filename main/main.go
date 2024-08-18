e88484fcca8db66edfa11492f50718893a02af04
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-08-18 16:54:32 +0800

    Add files via upload

--- main/main.go
+++ main/main.go
@@ -1,28 +1,6 @@
 package main
 
-import (
-	"strings"
-)
-
 func main() {
-	haystack := "helloworld"
-	needle := "l"
-	func(s string) {
-		for i, j := 0, 0; i < len(s); i += j + 1 {
-			if j = strings.Index(s[i:], needle); j == -1 {
-				return
-			}
-			print(i+j, " ")
-		}
-	}(haystack)
-
-	println()
-	func(s string) {
-		for i := len(s); i > 0; {
-			if i = strings.LastIndex(s[:i], needle); i != -1 {
-				print(i, " ")
-			}
-		}
-	}(haystack)
-	println()
+	a := 1_123
+	println(a)
 }
