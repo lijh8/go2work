28ab10ee3e94ed97c2dfd9277099d4b29a80d840
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-08-18 17:52:53 +0800

    Add files via upload

--- main/main.go
+++ main/main.go
@@ -1,6 +1,33 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
 func main() {
-	a := 1_123
-	println(a)
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+
+	n, err := writer.WriteString("Enter a line: ")
+	if err != nil {
+		fmt.Println(err, n)
+		return
+	}
+	writer.Flush()
+
+	line, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Println(err, line)
+		return
+	}
+
+	msg := fmt.Sprintf("You entered: %s\n", line)
+	n, err = writer.WriteString(msg)
+	if err != nil {
+		fmt.Println(err, n)
+		return
+	}
+	writer.Flush()
 }
