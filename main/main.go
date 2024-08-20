670c81b5ef570ffdd82280a3e5724c3007b1e3ce
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-08-20 08:27:11 +0800

    Add files via upload

--- main/main.go
+++ main/main.go
@@ -1,18 +1,31 @@
 package main
 
-import (
-	"log"
-	"log2"
-)
+/*
+func main2() {
+	a := 10
+	func() {
+		a++
+	}()
+	println(a) // 11
+
+}
+*/
 
 func main() {
-	logfile := "logfile.log"
-	logfd, err := log2.InitLog(logfile)
-	if err == nil {
-		defer logfd.Close()
+
+	values := []int{1, 2, 3}
+
+	// problem
+	for _, val := range values {
+		func() {
+			println(val)
+		}()
 	}
 
-	log.Println("aaa")
-	log.Println("bbb")
-
+	// temporary fix
+	// for _, val := range values {
+	// 	func(val int) { // val, not anymore. Fixed
+	// 		println(val)
+	// 	}(val) // val, not anymore. Fixed
+	// }
 }
