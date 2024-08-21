c573e1d5b5217d4313c03edbe4597b4815b13e18
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-08-22 05:36:14 +0800

    Add files via upload

--- main/main.go
+++ main/main.go
@@ -1,36 +1,28 @@
 package main
 
-type Yield[V any] func(V) bool
-type Yield2[K, V any] func(K, V) bool
+import "iter"
 
-func sliceDemo[K int, V any](s []V) func(Yield2[K, V]) {
-	return func(yield Yield2[K, V]) {
-		for i, v := range s {
-			if !yield(K(i), v) {
+// func Countdown(v int) func(func(int) bool) { // without iter package
+func Countdown(v int) iter.Seq[int] { // with iter package
+	return func(yield func(int) bool) {
+		for i := v; i >= 1; i-- {
+			if !yield(i) {
 				break
 			}
 		}
 	}
 }
 
-func mapDemo[K comparable, V any](s map[K]V) func(Yield2[K, V]) {
-	return func(yield Yield2[K, V]) {
-		for i, v := range s {
-			if !yield(i, v) {
-				break
-			}
-		}
-	}
-}
-
+// yield returning false and early exit
 func main() {
-	s := []string{"aaa", "bbb", "ccc"}
-	for i, v := range sliceDemo(s) {
-		println(i, v)
+	n := 5
+	value := 3
+	found := false
+	for x := range Countdown(n) {
+		if x == value {
+			found = true
+			break // this make the yield return false and exit early
+		}
 	}
-
-	m := map[string]int{"aaa": 10, "bbb": 20, "ccc": 30}
-	for i, v := range mapDemo(m) {
-		println(i, v)
-	}
+	println(found)
 }
