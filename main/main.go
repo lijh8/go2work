7ed5dd7abb9802f9a38b8bc36389545687cfde14
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-08-21 22:51:01 +0800

    Add files via upload

--- main/main.go
+++ main/main.go
@@ -5,8 +5,19 @@ import "iter"
 // func Countdown(v int) func(func(int) bool) { // without iter package
 func Countdown(v int) iter.Seq[int] { // with iter package
 	return func(yield func(int) bool) {
-		for i := v; i >= 0; i-- {
+		for i := v; i >= 1; i-- {
 			if !yield(i) {
+				break
+			}
+		}
+	}
+}
+
+// func DemoSeq2[T any](s []T) func(func(int, T) bool) { // without iter package
+func DemoSeq2[T any](s []T) iter.Seq2[int, T] { // with iter package
+	return func(yield func(int, T) bool) {
+		for i, v := range s {
+			if !yield(i, v) {
 				break
 			}
 		}
@@ -18,10 +29,17 @@ func main() {
 	for x := range Countdown(n) {
 		println(x)
 	}
+
+	s := []string{"aaa", "bbb", "ccc"}
+	for i, v := range DemoSeq2(s) {
+		println(i, v)
+	}
 }
 
 // output:
 // 3
 // 2
 // 1
-// 0
+// 0 aaa
+// 1 bbb
+// 2 ccc
