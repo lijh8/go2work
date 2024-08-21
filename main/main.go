91f368ed684aeaeb6142baa213d450b1be860b84
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-08-22 01:17:49 +0800

    Add files via upload

--- main/main.go
+++ main/main.go
@@ -7,39 +7,20 @@ func Countdown(v int) iter.Seq[int] { // with iter package
 	return func(yield func(int) bool) {
 		for i := v; i >= 1; i-- {
 			if !yield(i) {
+				println("yield: ", i)
 				break
 			}
-		}
-	}
-}
-
-// func DemoSeq2[T any](s []T) func(func(int, T) bool) { // without iter package
-func DemoSeq2[T any](s []T) iter.Seq2[int, T] { // with iter package
-	return func(yield func(int, T) bool) {
-		for i, v := range s {
-			if !yield(i, v) {
-				break
-			}
+			println("func: ", i)
 		}
 	}
 }
 
 func main() {
-	n := 3
+	n := 5
 	for x := range Countdown(n) {
 		println(x)
-	}
-
-	s := []string{"aaa", "bbb", "ccc"}
-	for i, v := range DemoSeq2(s) {
-		println(i, v)
+		if x == 3 {
+			return // this make the yield return false and exit early
+		}
 	}
 }
-
-// output:
-// 3
-// 2
-// 1
-// 0 aaa
-// 1 bbb
-// 2 ccc
