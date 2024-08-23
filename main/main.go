d9ad2fb5b9c1c0e21ad8f3788cf3b2dc3027430a
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-08-24 00:33:35 +0800

    Add files via upload

--- main/main.go
+++ main/main.go
@@ -1,38 +1,4 @@
 package main
 
-type Yield[V any] func(V) bool
-type Yield2[K comparable, V any] func(K, V) bool
-type Seq[E any] func(Yield[E])
-type Seq2[K comparable, V any] func(Yield2[K, V])
-
-// not importing iter, so can't use the named types
-func All2[E any](s []E) func(func(E) bool) {
-	return func(yield func(E) bool) {
-		for _, v := range s {
-			if !yield(v) {
-				return
-			}
-		}
-	}
-}
-
-func All2_b[E any](s []E) Seq[E] {
-	return func(yield Yield[E]) {
-		for _, v := range s {
-			if !yield(v) {
-				return
-			}
-		}
-	}
-}
-
 func main() {
-	s := []string{"aaa", "bbb", "ccc"}
-	for i := range All2(s) {
-		println(i)
-	}
-	for i := range All2_b(s) {
-		println(i)
-	}
-
 }
