14622fea1156f6698dac5aaa27802d24ecf2d5e6
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-08-25 08:26:50 +0800

    Add files via upload

--- main/main.go
+++ main/main.go
@@ -1,12 +1,31 @@
 package main
 
-import "fmt"
+/*
+#include <stdlib.h>
+#include <string.h>
+*/
+import "C"
+import (
+	"fmt"
+	"unsafe"
+)
 
 func main() {
-	a := [...]int{1, 2, 3}
-	t := []int{10, 20, 30, 40, 50}
-	copy(a[:], t)
-	// copy(a, t[:]) //no
-	copy(t, a[:]) //no
-	fmt.Println(t, a)
+
+	// cgo: pass strings to and from C function
+	// char *strncpy2(char *dest, const char *src, size_t n);
+
+	buf := [32]byte{}
+	buflen := len(buf)
+	str := "hello"
+	cs := C.CString(str)
+	defer C.free(unsafe.Pointer(cs))
+
+	C.strncpy(
+		(*C.char)(unsafe.Pointer(&buf)),
+		cs,
+		C.size_t(buflen)-1)
+
+	fmt.Println(string(buf[:]))
+
 }
