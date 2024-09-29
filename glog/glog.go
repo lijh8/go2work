9913013f690f55eea4bc84c43de9239275e7e1c2
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-09-30 01:05:31 +0800

    Add files via upload

--- glog/glog.go
+++ glog/glog.go
@@ -10,16 +10,17 @@ import (
 )
 
 // $ cd /path/to/mymodule
-// $ go get github.com/golang/glog
-// $ go mod tidy # generates go.sum
+// $ go get github.com/golang/glog  # it will modify go.mod, go.sum
+// $ go mod tidy  # it will modify go.sum
 
 // $ go build
-// $ ./hello -log_dir=/path/to/log
+// $ ./hello -log_dir=/path/to/logs
+// $ go clean
 
 func main() {
 	// if user provides log_dir, it should exist.
 	// if user does not, a subdir is created.
-	subDir := "./log"
+	subDir := "./logs"
 	exePath, _ := os.Executable()
 	realPath, _ := filepath.EvalSymlinks(exePath)
 	logDir := filepath.Dir(realPath)
@@ -35,7 +36,7 @@ func main() {
 		}
 		flag.Set("log_dir", logDir)
 	}
-	// glog.MaxSize = 1024 * 1024 * 5 // for test
+	glog.MaxSize = 1024 * 1024 * 5 // 1.8 G default, for test
 
 	defer glog.Flush()
 
@@ -46,3 +47,17 @@ func main() {
 		// glog.Fatal("FATAL!") // this will exit the program
 	}
 }
+
+/*
+$ cat glog.ljhs-Mac-mini.ljh.log.INFO.20240930-003828.19717
+Log file created at: 2024/09/30 00:38:28
+Running on machine: ljhs-Mac-mini
+Binary: Built with gc go1.23.1 for darwin/amd64
+Previous log: logs/glog.ljhs-Mac-mini.ljh.log.INFO.20240930-003826.19717
+Log line format: [IWEF]mmdd hh:mm:ss.uuuuuu threadid file:line] msg
+W0930 00:38:28.781964   19717 glog.go:44] WARNING!
+E0930 00:38:28.795429   19717 glog.go:45] ERROR!
+I0930 00:38:28.795465   19717 glog.go:43] INFO!
+W0930 00:38:28.795471   19717 glog.go:44] WARNING!
+$
+*/
