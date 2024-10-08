7ca77ffc0cff7ebe3e034def5de2609af211bf27
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-10-08 21:40:14 +0800

    Add files via upload

--- main/main.go
+++ main/main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"log"
-	"reflect"
 )
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
-	s := "abc"
-	var s2 string = "abcde"
-	log.Println(reflect.TypeOf(s))
-	log.Println(reflect.TypeOf(s2))
+	m := map[string]int{"aaa": 10, "bbb": 20}
+	m["ccc"] = 30
+	m["ddd"] = 40
+	log.Println(m)
+
 }
