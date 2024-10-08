2780dfb87c8000c85e9546b84e682dbb9bff2eeb
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-10-09 05:05:08 +0800

    Add files via upload

--- main/main.go
+++ main/main.go
@@ -1,18 +1,12 @@
 package main
 
-import (
-	"log"
-)
+import "log"
 
 func init() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 }
 
 func main() {
-
-	m := map[string]int{"aaa": 10, "bbb": 20}
-	m["ccc"] = 30
-	m["ddd"] = 40
-	log.Println(m)
+	log.Println()
 
 }
