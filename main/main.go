caaa00fdda361fb4218fe49b1962643ef87fe387
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-10-10 21:55:37 +0800

    Add files via upload

--- main/main.go
+++ main/main.go
@@ -1,6 +1,22 @@
 package main
 
+import (
+	"strings"
+)
+
+func lengthOfLongestSubstring(s string) int {
+	m := 0
+	for i, j := 0, 0; j < len(s); j++ {
+		if k := strings.IndexByte(s[i:j], s[j]); k != -1 {
+			i = i + k + 1
+		}
+		m = max(m, j-i+1)
+	}
+	return m
+}
+
 func main() {
-	LOG()
-	LOG("")
+	LOG(lengthOfLongestSubstring("abcabcbb")) // 3
+	LOG(lengthOfLongestSubstring("bbbbb"))    // 1
+	LOG(lengthOfLongestSubstring("pwwkew"))   // 3
 }
